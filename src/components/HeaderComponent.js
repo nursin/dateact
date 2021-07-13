fa960418ed7251236754978ce289902de65fb3fd
Bobby Keel <bobbykeel76@gmail.com>
2021-07-13 13:18:38 -0400

    Header completed

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavBarToggler, NavItem, Jumbotron, Button, Modal, ModalHeader, Form, FormGroup, Input, Label } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, Jumbotron, Button, Collapse, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -12,13 +12,13 @@ class Header extends Component {
     };
   }
 
-  toggleNav() {
+  toggleNav = () => {
     this.setState({
       isNavOpen: !this.state.isNavOpen
     });
   }
 
-  toggleModal() {
+  toggleModal = () => {
     this.setState({
       isModalOpen: !this.state.isModalOpen
     })
@@ -30,21 +30,66 @@ class Header extends Component {
         <Jumbotron fluid>
           <div className="container">
             <div className="row">
-              <div class="col-2 mt-2">
+              <div class="col-12 col-sm-2 col-lg-1 mb-3 mb-sm-0 text-center align-self-center">
                 <a id="logo" href="index.html"><i className="fa fa-transgender-alt fa-5x"/></a>
               </div>
-              <div className="col text-left">
+              <div className="col ml-sm-4 ml-md-0 ml-lg-4 ml-xl-0 text-center text-sm-left">
                 <h1>Date Act</h1>
-                <h2>Spice up your date life</h2>
+                <h2 id="slogan">Spice up your date life</h2>
               </div>
+              {/* <!-- This button will open a share modal so the user can select messages, social, email and such --> */}
+              <span class="col-md-4 col-xl-2 mt-4 text-center">
+                <Button className="btn btn-warning btn-lg text-black" outline onClick={this.toggleModal}>Share With Friends</Button>
+              </span>
             </div>
           </div>
         </Jumbotron>
         <Navbar dark sticky="top" expand="md">
           <div className="container">
-
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <i className="fa fa-home fa-lg"/> Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <i className="fa fa-info fa-lg"/> About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <i className="fa fa-address-card fa-lg"/> Contact
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </Navbar>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader className="bg-gradient text-center" toggle={this.toggleModal}><h2 className="text-white">Share on social media</h2></ModalHeader>
+          <ModalBody>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-12 text-center">
+                  <a href="http://facebook.com/" class="btn button-hover-bg-gradient bg-blue text-white" target="_blank">Share on <i class="fa fa-facebook fa-2x"></i>acebook</a>
+                </div>
+                <div class="col-12 text-center mt-2">
+                  <a href="http://twitter.com/" class="btn button-hover-bg-gradient bg-blue text-white" target="_blank">Share on <i class="fa fa-twitter fa-2x"></i>Twitter</a>
+                </div>
+                <div class="col-12 text-center mt-2">
+                  <a href="http://instagram.com/" class="btn button-hover-bg-gradient bg-blue text-white" target="_blank">Share on <i class="fa fa-instagram fa-2x"></i> Instagram</a>
+                </div>
+                <div class="col-12 text-center mt-2">
+                  <a href="http://youtube.com/" class="btn button-hover-bg-gradient bg-blue text-white" target="_blank">Share on <i class="fa fa-youtube fa-2x"></i> YouTube</a>
+                </div>
+              </div>
+            </div>
+          </ModalBody>
+        </Modal>
       </React.Fragment>
     )
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,9 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Home from './HomeComponent';
 import Header from './HeaderComponent'
+import About from './AboutComponent';
+import Contact from './ContactComponent';
+
 
 class Main extends Component {
   render() {
@@ -12,11 +15,26 @@ class Main extends Component {
         <Home />
       );
     }
+
+    const AboutPage = () => {
+      return (
+        <About />
+      );
+    }
+
+    const ContactPage = () => {
+      return (
+        <Contact />
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
+          <Route path='/aboutus' component={AboutPage} />
+          <Route path='/contactus' component={ContactPage} />
           <Redirect to='/home' />
         </Switch>
       </div>
