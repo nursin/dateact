04d819e89caf0148ea56913fbf5982084bff732f
Bobby Keel <bobbykeel76@gmail.com>
2021-07-18 19:44:14 -0400

    react router attempt fix for refresh 404 error

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -50,7 +50,7 @@ class Header extends Component {
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
-                  <NavLink className="nav-link" to="/home">
+                  <NavLink className="nav-link" to="/">
                     <i className="fa fa-home fa-lg"/> Home
                   </NavLink>
                 </NavItem>
