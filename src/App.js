fcd6b484102f99e01c761d6d9dabb6b7c39a434a
Bobby Keel <bobbykeel76@gmail.com>
2021-07-19 03:35:05 -0400

    react router attempt fix for refresh 404 error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   render() {    
     return (
-      <BrowserRouter basename="/home">
+      <BrowserRouter basename="/">
         <div className="App">
           <Main />
         </div>
